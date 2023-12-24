938fe2d5e013b526157df234ee549fdd74370ae9
vuralkayracetintas <vuralkayracetintass@gmail.com>
2023-12-24 22:31:11 +0300

    delete code and viewcontroller

--- layouts/layouts/ViewController.swift
+++ layouts/layouts/ViewController.swift
@@ -13,17 +13,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let width   = view.frame.size.width
-        let height = view.frame.size.height
-        
-        let myLabel = UILabel()
-        myLabel.text = "Test Label"
-        myLabel.textColor = UIColor.red
-        myLabel.textAlignment = .center
-        
-        myLabel.frame = CGRect(x: width * 0.5 - width * 0.8 /  2 , y: height * 0.5, width: 100, height: 100)
-        view.addSubview(myLabel)
-        
     }
 
 
