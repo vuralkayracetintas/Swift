204db3d0535e5d3f6b31c841c4004da42df9260c
vuralkayracetintas <vuralkayracetintass@gmail.com>
2023-12-27 14:26:53 +0300

    life cycle

--- SegueApp/SegueApp/ViewController.swift
+++ SegueApp/SegueApp/ViewController.swift
@@ -15,7 +15,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("viewDidLoad function called")
+        
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear function called") //controller kayboldu
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear function called") // kaybolacak
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        nameTextField.text = ""
+        print("viewWillAppear function called") // gorunum gozukecek
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        print("viewDidAppear function called") // gorunum gozuktu
+    }
+    
+    
+    
+    
 
     @IBAction func dataTransferButton(_ sender: Any) {
         userName = nameTextField.text!
