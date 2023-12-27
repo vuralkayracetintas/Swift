a0985531b06ecd0f5e8063d1d031e35def0d9ff1
vuralkayracetintas <vuralkayracetintass@gmail.com>
2023-12-27 11:51:09 +0300

    usage userdefaults

---
+++ BirthdayNoteTaker/BirthdayNoteTaker/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  BirthdayNoteTaker
+//
+//  Created by Vural Kayra Çetintaş on 27.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var nameText: UILabel!
+    @IBOutlet weak var birthdayText: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let savedName = UserDefaults.standard.object(forKey: "name")
+        let savedBirthday = UserDefaults.standard.object(forKey: "birhday")
+        
+        
+        //Casting as?  - as!
+        if let newName = savedName as? String{
+            
+            nameText.text = "Name : \(newName)"
+        }
+        
+        if let newBirthday = savedBirthday as? String{
+            birthdayText.text = "Birthday : \(newBirthday)"
+
+            
+        }
+        
+    }
+
+
+    @IBAction func saveButton(_ sender: Any) {
+        let name = nameTextField.text
+        let birhday = birthdayTextField.text
+        
+        
+        UserDefaults.standard.set(name, forKey:"name")
+        UserDefaults.standard.set(birhday, forKey:"birhday")
+        
+        nameText.text = "Name : \(name!)"
+        birthdayText.text = "Birthday : \(birhday!)"
+
+        
+
+    }
+}
+
